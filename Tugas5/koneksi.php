ffee9ba4d50a4539bbea7ffdefcf642e1f09e428
Ferdinand Putra Gumilang Silalahi <gumilangsilalahi3@gmail.con>
2022-12-21 11:13:20 +0700

    edited files

--- Tugas5/koneksi.php
+++ Tugas5/koneksi.php
@@ -1,8 +1,7 @@
 <?php
-$host = "localhost"; // Nama hostnya
-$username = "root"; // Username
-$password = ""; // Password (Isi jika menggunakan password)
-$database = "mynotescode"; // Nama databasenya
-// Koneksi ke MySQL dengan PDO
+$host = "localhost"; 
+$username = "root"; $password = ""; 
+$database = "mynotescode"; 
+
 $pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
 ?>
